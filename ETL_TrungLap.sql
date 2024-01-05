d64ccd1e6d6c15eac61875a3f34ef143d4f182bc
thainq1402 <nguyenquocthaihn142@gmail.com>
2024-01-05 19:41:13 +0700

    Nâng cấp Pro_ID_CapBac

---
+++ ETL_TrungLap.sql
@@ -0,0 +1,10 @@
+CREATE DEFINER=`tuyendungUser`@`%` PROCEDURE `ETL_ID_CapBac`()
+BEGIN
+    UPDATE Stg_ThongTin t1 
+    JOIN (
+        SELECT TenCV, ID_TinhThanh, ID_CongTy, COUNT(Link) as TrungLap
+        FROM Stg_ThongTin t2 
+        GROUP BY ID_TinhThanh, ID_CongTy, TenCV
+    ) t2 ON t1.ID_CongTy = t2.ID_CongTy AND t1.ID_TinhThanh = t2.ID_TinhThanh AND t1.TenCV = t2.TenCV 
+    SET t1.CountTin = t2.TrungLap, t1.TrangThaiTrungLap = CASE WHEN t2.TrungLap > 1 THEN 1 ELSE 0 END;
+END;
